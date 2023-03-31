f76310eec5df46afc75a8b2b0db852a6cdd556dc
kajsa <kajsa.jernetz@gmail.com>
2023-03-31 15:49:45 +0200

    Predicts volatility

---
+++ garch_test.py
@@ -0,0 +1,78 @@
+import pandas as pd
+
+df = pd.read_csv("example_prices.csv")
+df
+
+import math
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+data = df.diamonds
+data['Return'] = 100 * (data.pct_change())
+
+data.dropna(inplace=True)
+
+fig,ax = plt.subplots(figsize=(8,4))
+ax.spines[['top','right']].set_visible(False)
+plt.plot(data['Return'], label = 'Daily Returns')
+plt.legend(loc='upper right')
+plt.title('Daily Returns Over Time')
+
+daily_volatility = data['Return'].std()
+
+monthly_volatility = math.sqrt(21) * daily_volatility
+
+annual_volatility = math.sqrt(252) * daily_volatility
+
+from tabulate import tabulate
+
+print(tabulate([['Diamonds',daily_volatility,monthly_volatility,annual_volatility]],headers = ['Daily Volatility %', 'Monthly Volatility %', 'Annual Volatility %'],tablefmt = 'fancy_grid',stralign='center',numalign='center',floatfmt=".2f"))
+
+
+# Build GARCH
+from arch import arch_model
+from arch.__future__ import reindexing
+
+
+garch_model = arch_model(data['Return'][1::], p = 1, q = 1,
+                      mean = 'constant', vol = 'GARCH', dist = 'normal')
+
+gm_result = garch_model.fit(disp='off')
+print(gm_result.params)
+
+print('\n')
+
+gm_forecast = gm_result.forecast(horizon = 5)
+print(gm_forecast.variance[-1:])
+
+
+# Rolling predictions
+rolling_predictions = []
+test_size = 365
+
+for i in range(test_size):
+    train = data['Return'][1::][:-(test_size-i)]
+    model = arch_model(train, p=1, q=1)
+    model_fit = model.fit(disp='off')
+    pred = model_fit.forecast(horizon=1)
+    rolling_predictions.append(np.sqrt(pred.variance.values[-1,:][0]))
+   
+rolling_predictions = pd.Series(rolling_predictions, index=data['Return'][1::].index[-365:])
+
+fig,ax = plt.subplots(figsize=(10,4))
+ax.spines[['top','right']].set_visible(False)
+plt.plot(rolling_predictions)
+plt.title('Rolling Prediction')
+
+# Accuracy test 
+
+fig,ax = plt.subplots(figsize=(13,4))
+ax.grid(which="major", axis='y', color='#758D99', alpha=0.3, zorder=1)
+ax.spines[['top','right']].set_visible(False)
+plt.plot(data['Return'][1::][-365:])
+plt.plot(rolling_predictions)
+plt.title(' Volatility Prediction - Rolling Forecast')
+plt.legend(['True Daily Returns', 'Predicted Volatility'])
+
